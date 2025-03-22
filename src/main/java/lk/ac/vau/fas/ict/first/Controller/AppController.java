22653677a159aa550d46ac21281ff5e761897c75
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-03-22 15:40:57 +0530

    feat: add basic AppController with message endpoint

---
+++ src/main/java/lk/ac/vau/fas/ict/first/Controller/AppController.java
@@ -0,0 +1,16 @@
+package lk.ac.vau.fas.ict.first.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+    @GetMapping("/msg")
+    public String myMessage() {
+        return "Hello SpringBoot";
+    }
+    
+}
