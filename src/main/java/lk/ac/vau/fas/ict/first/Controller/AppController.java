6c60c30693425c0e489c0c66d6376e90d4a09259
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-03-29 11:55:54 +0530

    feat: add endpoint to return a list of all students

--- src/main/java/lk/ac/vau/fas/ict/first/Controller/AppController.java
+++ src/main/java/lk/ac/vau/fas/ict/first/Controller/AppController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lk.ac.vau.fas.ict.first.Model.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +24,21 @@ public class AppController {
     Student Bob = new Student("2021ICT01","Bob Marely",23,"IT", 3.21);
     Student Amal = new Student("2020ICT02", "Amal Perera",24,"AMC",3.41);
 	Student Kamal = new Student("2022ICT03", "Kamal Gunasinghe",22,"AMC",2.91);
+    List<Student> students = new ArrayList<Student>();
 
     //A method to return a student
     @GetMapping("/student")
     public Student getStudent() {
         return Bob;
+    }
+
+    //return multiple student
+    @GetMapping("/all-students")
+    public List<Student> getAll() {
+        students.add(Bob);
+        students.add(Amal);
+        students.add(Kamal);
+        return students;
     }
 
     @GetMapping("/msg")
